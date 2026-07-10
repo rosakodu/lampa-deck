8673e46df0a3a9715b2cd0b9b412f8762f89165b
rosakodu <rosakodu@users.noreply.github.com>
2026-07-10 05:31:54 +0300

    v1.0.0: Intercept TorrServer playback URLs and rewrite them to TorrServer-gst HLS transcoding endpoint

--- lampa/settings-overlay.js
+++ lampa/settings-overlay.js
@@ -72,6 +72,19 @@
               console.error('Failed to trigger play on backend:', err);
             });
         } else {
+          // Intercept TorrServer play URLs and rewrite them to use GStreamer HLS transcoding
+          if (data.url && data.url.indexOf('/play/') !== -1) {
+            const match = data.url.match(/\/play\/([a-fA-F0-9]+)\/(\d+)/);
+            if (match) {
+              const hash = match[1];
+              const index = match[2];
+              const hostMatch = data.url.match(/^(https?:\/\/[^\/]+)/);
+              const host = hostMatch ? hostMatch[1] : 'http://127.0.0.1:8090';
+              const newUrl = host + '/gst/' + hash + '/master.m3u8?index=' + index;
+              console.log('Rewriting TorrServer play URL for GStreamer HLS transcoding:', data.url, '->', newUrl);
+              data.url = newUrl;
+            }
+          }
           // Play using native Lampa player
           originalPlay.call(window.Lampa.Player, data);
         }
